7596d35a3cd64b676931caa77434e002c49f5ced
Ethan Brooks <ethanabrooks@gmail.com>
2020-06-23 13:13:39 -0400

    got docker file working

---
+++ entrypoint.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+python main.py --policy SAC --max-time-steps 100000
+
---
+++ env.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+export RUN_DB_PATH="$HOME/IdeaProjects/lab-notebook/runs"
+export RUN_LOG_DIR="$HOME/IdeaProjects/lab-notebook/.runs"
+export RUN_IMAGE_BUILD_PATH="$HOME/IdeaProjects/lab-notebook"
+export RUN_DOCKERFILE_PATH="$HOME/IdeaProjects/lab-notebook/Dockerfile"
+export RUN_IMAGE='lab-notebook-test-image'
+export RUN_CONFIG_SCRIPT='run_launcher.py'
+export RUN_CONFIG_SCRIPT_INTERPRETER='python3'
+export RUN_CONFIG_SCRIPT_INTERPRETER_ARGS='-c'
