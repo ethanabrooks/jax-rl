5ee3fd0c7b1fab55754b294b9ee4e1c01af01cc8
Ethan Brooks <ethanabrooks@gmail.com>
2020-07-16 13:24:20 -0400

    name image jax

--- env.sh
+++ env.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 
-export RUN_DB_PATH="$HOME/runs/sac"
+export RUN_DB_PATH="$HOME/runs/jax"
 export RUN_IMAGE_BUILD_PATH="$HOME/jax-rl/"
 export RUN_DOCKERFILE_PATH="$RUN_IMAGE_BUILD_PATH/Dockerfile"
-export RUN_IMAGE='sac'
+export RUN_IMAGE='jax'
 export RUN_CONFIG_SCRIPT='config_script.py'
 export RUN_CONFIG_SCRIPT_INTERPRETER='python3'
 export RUN_CONFIG_SCRIPT_INTERPRETER_ARGS='-c'
