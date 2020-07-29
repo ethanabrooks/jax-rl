f354ad957c02c0276bbd2c959a5c2bff57b2d844
Ethan Brooks <ethanabrooks@gmail.com>
2020-07-29 17:12:40 -0400

    break out medium_values in config

--- configs.py
+++ configs.py
@@ -5,16 +5,20 @@ def small_values(start, stop):
     return [j for i in range(start, stop) for j in ((10 ** -i), 5 * (10 ** -i))]
 
 
-def big_values(start, stop):
+def medium_values(start, stop):
     return [2 ** i for i in range(start, stop)]
+
+
+def big_values(start, stop):
+    return [j for i in range(start, stop) for j in ((10 ** i), 5 * (10 ** i))]
 
 
 search = dict(
     actor_freq=hp.choice("actor_freq", [1, 2, 3]),
-    batch_size=hp.choice("batch_size", big_values(6, 10)),
+    batch_size=hp.choice("batch_size", medium_values(6, 10)),
     learning_rate=hp.choice("learning_rate", small_values(2, 5)),
     seed=hp.randint("seed", 20),
-    start_time_steps=hp.choice("start_time_steps", big_values(3, 6)),
+    start_time_steps=hp.choice("start_time_steps", big_values(3, 5)),
     tau=hp.choice("tau", small_values(2, 5)),
     train_steps=hp.choice("train_steps", [1, 2, 3]),
 )
@@ -35,18 +39,7 @@ double = dict(
 )
 
 double_search = dict(
-    outer_actor_freq=hp.choice("actor_freq", [1, 2, 3]),
-    outer_batch_size=hp.choice("batch_size", big_values(6, 10)),
-    outer_learning_rate=hp.choice("learning_rate", small_values(2, 5)),
-    outer_seed=hp.randint("seed", 20),
-    outer_start_time_steps=hp.choice("start_time_steps", big_values(3, 6)),
-    outer_tau=hp.choice("tau", small_values(2, 5)),
-    outer_train_steps=hp.choice("train_steps", [1, 2, 3]),
-    actor_freq=hp.choice("actor_freq", [1, 2, 3]),
-    batch_size=hp.choice("batch_size", big_values(6, 10)),
-    learning_rate=hp.choice("learning_rate", small_values(2, 5)),
-    seed=hp.randint("seed", 20),
-    start_time_steps=hp.choice("start_time_steps", big_values(3, 6)),
-    tau=hp.choice("tau", small_values(2, 5)),
-    train_steps=hp.choice("train_steps", [1, 2, 3]),
+    max_time_steps=big_values(3, 5),
+    **search,
+    **{"outer_" + k: v for k, v in search.items()}
 )
